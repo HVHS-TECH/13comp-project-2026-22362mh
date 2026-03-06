4f5a7ae674334ce0e63f8af2ed934b50a90a9f70
Mio Hoffman <22362mh@hvhs.school.nz>
2026-03-06 15:14:07 +1300

    spell five friday 06/03

--- fb_io.mjs
+++ fb_io.mjs
@@ -11,7 +11,7 @@
 const COL_C = 'white';	    // These two const are part of the coloured
 const COL_B = '#820b8d';	//  console.log for functions scheme
 console.log('%c fb_io.mjs',
-            'color: blue; background-color: white;');
+    'color: blue; background-color: white;');
 
 //Importing the needed firebase functions
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
@@ -21,20 +21,20 @@ import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstati
 
 /***** GLOBAL VARIABLES *****/
 var fb_gamedb;
-var signedIn = false; 
+var signedIn = false;
 
 //Function for initialising firebase
-function fb_initialise(){
-const FB_GAMECONFIG = {
-  apiKey: "AIzaSyDuJnRiExxWkHuXgYW_9LpGaVLcfVYRIiE",
-  authDomain: "compgamefirebase-miohoffman.firebaseapp.com",
-  databaseURL: "https://compgamefirebase-miohoffman-default-rtdb.firebaseio.com",
-  projectId: "compgamefirebase-miohoffman",
-  storageBucket: "compgamefirebase-miohoffman.firebasestorage.app",
-  messagingSenderId: "879943944137",
-  appId: "1:879943944137:web:27014e6f84077c4b994a18",
-  measurementId: "G-2LQPB4K734"
-};
+function fb_initialise() {
+    const FB_GAMECONFIG = {
+        apiKey: "AIzaSyDuJnRiExxWkHuXgYW_9LpGaVLcfVYRIiE",
+        authDomain: "compgamefirebase-miohoffman.firebaseapp.com",
+        databaseURL: "https://compgamefirebase-miohoffman-default-rtdb.firebaseio.com",
+        projectId: "compgamefirebase-miohoffman",
+        storageBucket: "compgamefirebase-miohoffman.firebasestorage.app",
+        messagingSenderId: "879943944137",
+        appId: "1:879943944137:web:27014e6f84077c4b994a18",
+        measurementId: "G-2LQPB4K734"
+    };
 
     const FB_GAMEAPP = initializeApp(FB_GAMECONFIG);
     fb_gamedb = getDatabase(FB_GAMEAPP);
@@ -42,11 +42,11 @@ const FB_GAMECONFIG = {
 }
 
 //Has the user login with google
-function fb_login(){
+function fb_login() {
     const AUTH = getAuth();
     const PROVIDER = new GoogleAuthProvider();
     // The following makes Google ask the user to select the account
-PROVIDER.setCustomParameters({
+    PROVIDER.setCustomParameters({
         prompt: 'select_account'
     });
     signInWithPopup(AUTH, PROVIDER).then((result) => {
@@ -56,24 +56,24 @@ PROVIDER.setCustomParameters({
         loginStatus.innerHTML = "You have logged in!";
         signedIn = true;
     })
-    .catch((error) => {
-        console.log(error);
-    });
+        .catch((error) => {
+            console.log(error);
+        });
 }
 
-function getFormInput(){
+function getFormInput() {
     //If the user is signed in, they can input a username successfully and then it'll be displayed
-    if (signedIn == true){
+    if (signedIn == true) {
         //Getting the user's username from the HTML form
         var username = document.getElementById("username");
         username = username.value; //Making sure the username is the value or text that the user inputted.
         console.log(username);
 
         var usernameDisplay = document.getElementById("usernameDisplay");
-        usernameDisplay.innerHTML = "Your username is: " +  username;
+        usernameDisplay.innerHTML = "Your username is: " + username;
     }
     //If the user isn't signed in and tries to input a username, they get alerted.
-    else if (signedIn == false){
+    else if (signedIn == false) {
         alert("You have not logged in yet!");
     }
 }
