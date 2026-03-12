a13e6c84470f80ae65e05889d05d5cb130ec8837
Mio Hoffman <22362mh@hvhs.school.nz>
2026-03-12 13:29:34 +1300

    Created validation for the guess the number game and expanded the number range to 20

--- gtn.mjs
+++ gtn.mjs
@@ -1,7 +1,7 @@
 var correctAnswer;
 
 function getNumber(){
-    correctAnswer = Math.floor(Math.random() * 6) + 1;
+    correctAnswer = Math.floor(Math.random() * 20) + 1;
     console.log(correctAnswer);
 }
 
@@ -12,11 +12,25 @@ function getGuess(){
 
     if (guess == correctAnswer){
         console.log("You got it right!");
+        alert("You got it right!");
     }
-    else {
-        console.log("You are wrong.");
+    else if(guess <= 0){
+        alert("Put in a number between 1 and 20!");
+    }
+    else if(guess > 20){
+        alert("Put in a number between 1 and 20!")
+    }
+    else if (guess < correctAnswer){
+        console.log("Higher!");
+        alert("The number is higher!");
         var wrong = document.getElementById(guess);
-        wrong.style.color = 'red';
+        wrong.style.color = 'rgb(170, 0, 0)';
+    }
+    else if (guess > correctAnswer){
+        console.log("Lower!");
+        alert("The number is lower!");
+        wrong = document.getElementById(guess);
+        wrong.style.color = 'rgb(170, 0, 0)';
     }
 }
 
