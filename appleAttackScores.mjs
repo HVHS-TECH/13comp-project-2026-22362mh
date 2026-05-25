bd815c1e550209696d933dad46983ef4317b2c71
Mio Hoffman <22362mh@hvhs.school.nz>
2026-05-26 09:56:45 +1200

    Got the Apple Attack Leaderboard successfully working and displaying test scores with usernames

--- appleAttackScores.mjs
+++ appleAttackScores.mjs
@@ -11,23 +11,55 @@ import { username as userName } from "./login.mjs"
 import { fb_gamedb, fb_initialise } from "./fb_io.mjs";
 
 let uid;
-fb_initialise();
 
 function aa_readSorted() {
+    let scoreArray = [];
+
     let AppleAttackGameScores = "/gameScores/appleAttack"
-    let numberToRead = 5;
+    let numberToRead = 4;
     let sortkey = "score"
     const dbReference = query(ref(fb_gamedb, AppleAttackGameScores), orderByChild(sortkey), limitToFirst(numberToRead));
     get(dbReference).then((snapshot) => {
-        var fb_data = snapshot.val();
-        if (fb_data != null) {
-            console.log(fb_data);
-        } else {
-            console.log("No record found");
-        }
+       snapshot.forEach(function(userScoreSnapshot) {
+        var scores = userScoreSnapshot.val();
+        console.log(scores.score);
+        let userName = userScoreSnapshot.key;
+        let userScore = scores.score;
+        let scoreObject = {[userName]: userScore};
+        scoreArray.push(scoreObject);
+       });
+       console.log(scoreArray);
+       displayScores(scoreArray);
     }).catch((error) => {
         console.log(error);
     });
+}
+
+function displayScores(scoreArray){
+    let firstPlace = document.getElementById("first_place");
+    let firstUserName = Object.keys(scoreArray[0]);
+    let firstUserScore = Object.values(scoreArray[0]);
+    firstPlace.innerHTML = "1. " + firstUserName + " : " + firstUserScore;
+
+    let secondPlace = document.getElementById("second_place");
+    let secondUserName = Object.keys(scoreArray[1]);
+    let secondUserScore = Object.values(scoreArray[1]);
+    secondPlace.innerHTML = "2. " + secondUserName + " : " + secondUserScore;
+
+    let thirdPlace = document.getElementById("third_place");
+    let thirdUserName = Object.keys(scoreArray[2]);
+    let thirdUserScore = Object.values(scoreArray[2]);
+    thirdPlace.innerHTML = "3. " + thirdUserName + " : " + thirdUserScore;
+
+    let fourthPlace = document.getElementById("fourth_place");
+    let fourthUserName = Object.keys(scoreArray[3]);
+    let fourthUserScore = Object.values(scoreArray[3]);
+    fourthPlace.innerHTML = "4. " + fourthUserName + " : " + fourthUserScore;
+
+    let fifthPlace = document.getElementById("fifth_place");
+    let fifthUserName = Object.keys(scoreArray[4]);
+    let fifthUserScore = Object.values(scoreArray[4]);
+    fifthPlace.innerHTML = "5. " + fifthUsername + " : " + fifthUserScore;
 }
 
 export {
